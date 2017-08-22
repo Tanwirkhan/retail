a407ca0740e3171195dcc2db0693d1a5993d2bca
Tanwirkhan <tkhan.kiit@gmail.com>
2017-08-22 07:35:14 +0530

    Initial commit

---
+++ src/main/scala/DailyRevenueAndOrderItems.scala
@@ -0,0 +1,59 @@
+/**
+  * Created by Tanwir on 14-06-2017.
+  */
+import com.typesafe.config.ConfigFactory
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object DailyRevenueAndOrderItems {
+
+  def main(args:Array[String]): Unit={
+    val conf = new SparkConf().setMaster("local").setAppName("Total Revenue per day")
+    val sc = new SparkContext(conf)
+    val inputBaseDir = args(0)
+    val outputpath = args(1)
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    //again sc has a API called hadoopConfiguration. If you run this
+    // then fs is of type file system.
+    //and we need to do FileSystem.get and pass the configuration to it(sc.hadoopConfiguration).
+
+
+    //Now if I want to use exists, I need to pass object of type path but our inputBaseDir and outputpath
+    //is of type string. So we to convert the string into path object and then we have to use fs.exists to
+    //compare if the path exists or not and then we have to validate. It returns boolean so we can use !exists
+    //and then raise some exception.
+
+    if(!fs.exists(new Path(inputBaseDir))) {
+      println("base directory doesnt exist")
+      return
+    }
+
+    //Similarly if output directory already exists , for this purpose i just want to delete.
+
+    val op = new Path(outputpath )
+    if(fs.exists(op)){
+      fs.delete(op,true)
+    }
+    //so if the output path exists then it will just delete and it will try to create new directory to copy the data.
+
+    val orders = sc.textFile(inputBaseDir + "/orders")
+    val orderFiltered = orders.filter(rec=> rec.split(",")(3)=="COMPLETE"||rec.split(",")(3)=="CLOSED")
+    val ordersMap = orderFiltered.map(rec => (rec.split(",")(0).toInt, rec.split(",")(1)))
+    val order_item = sc.textFile(inputBaseDir + "/order_items")
+    val order_itemMap = order_item.map(rec => (rec.split(",")(1).toInt, rec.split(",")(4).toDouble))
+    val ordersjoin = ordersMap.join(order_itemMap)
+    val ordersJoinMap = ordersjoin.map(rec => rec._2)
+    //RDD[(String,Int)]
+    val ordersJoinMapABK = ordersJoinMap.aggregateByKey((0.0,0))(
+      (intAgg : (Double,Int), intVal :Double) => (intAgg._1 + intVal, intAgg._2+1),
+      //(60000,60)(40000,40)
+      (totAgg: (Double,Int), totVal: (Double,Int))=>
+        (totAgg._1 + totVal._1, totAgg._2 + totVal._2)
+    )
+    //RDD[(String,(Double, Int)]
+    //100000,100
+    ordersJoinMapABK.saveAsTextFile(outputpath)
+  }
+
+}
---
+++ src/main/scala/Medical.scala
@@ -0,0 +1,55 @@
+/**
+  * Created by Tanwir on 14-06-2017.
+  */
+import com.typesafe.config.ConfigFactory
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object DailyRevenue {
+
+  def main(args:Array[String]): Unit={
+    val conf = new SparkConf().setMaster("local").setAppName("Total Revenue per day")
+    val sc = new SparkContext(conf)
+    val inputBaseDir = args(0)
+    val outputpath = args(1)
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    //again sc has a API called hadoopConfiguration. If you run this
+    // then fs is of type file system.
+    //and we need to do FileSystem.get and pass the configuration to it(sc.hadoopConfiguration).
+
+
+    //Now if I want to use exists, I need to pass object of type path but our inputBaseDir and outputpath
+    //is of type string. So we to convert the string into path object and then we have to use fs.exists to
+    //compare if the path exists or not and then we have to validate. It returns boolean so we can use !exists
+    //and then raise some exception.
+
+    if(!fs.exists(new Path(inputBaseDir))) {
+      println("base directory doesnt exist")
+      return
+    }
+
+    //Similarly if output directory already exists , for this purpose i just want to delete.
+
+    val op = new Path(outputpath )
+    if(fs.exists(op)){
+      fs.delete(op,true)
+    }
+    //so if the output path exists then it will just delete and it will try to create new directory to copy the data.
+
+    val orders = sc.textFile(inputBaseDir + "/medical_disease")
+    val orderFiltered = orders.filter(rec=> rec.split(",")(1)=="MALIGN")
+    val ordersMap = orderFiltered.map(rec => (rec.split(",")(0), rec.split(",")(1), rec.split(",")(2),rec.split(",")(3)))
+    val order_item = sc.textFile(inputBaseDir + "/medical_prescribed")
+    val orderFiltered1 = order_item.filter(rec=> rec.split(",")(3)=="MIT")
+    val order_itemMap = orderFiltered1.map(rec => (rec.split(",")(0), rec.split(",")(1),rec.split(",")(2),rec.split(",")(3),rec.split(",")(4)))
+    val orders_join = ordersMap.join(order_itemMap)
+   // val ordersJoinMap = ordersjoin.map(rec => rec._2)
+    //val ordersJoinMapRBK = ordersJoinMap.reduceByKey((agg, value) => agg+value)
+    // val orders1 =  ordersJoinMapRBK.map(rec=> rec._1 +"\t" + rec._2)
+    //   orders1.saveAsTextFile(outputpath)
+   // ordersJoinMapRBK.map(rec => rec._1 +"\t" + rec._2).saveAsTextFile(outputpath)
+    //ordersJoinMapRBK.map(rec=>rec.1.saveAsTextFile(outputpath)
+  }
+
+}
